72bb644a7ef54716e4db77593d1c8511b6c4c1b6
Johannes Weißl <jargon@molb.org>
2018-05-01 23:15:03 +0200

    namazu: Add upstream patch to fix search error due to memory corruption
    See http://cvs.namazu.org/Development/namazu/nmz/search.c?r1=1.116&r2=1.117&view=patch

--- Formula/namazu.rb
+++ Formula/namazu.rb
@@ -17,6 +17,8 @@ class Namazu < Formula
 
   depends_on "kakasi" if build.with? "japanese"
 
+  patch :DATA
+
   resource "text-kakasi" do
     url "https://cpan.metacpan.org/authors/id/D/DA/DANKOGAI/Text-Kakasi-2.04.tar.gz"
     mirror "http://search.cpan.org/CPAN/authors/id/D/DA/DANKOGAI/Text-Kakasi-2.04.tar.gz"
@@ -54,3 +56,73 @@ class Namazu < Formula
     assert_match /#{data_file}/, search_result
   end
 end
+
+__END__
+From 9029688dad93777335c530c9574d1fb208c79728 Mon Sep 17 00:00:00 2001
+From: NOKUBI Takatsugu <knok@daionet.gr.jp>
+Date: Fri, 26 Aug 2011 06:20:22 +0000
+Subject: [PATCH] Changed to use memmove instead of strcpy with overwrapped
+ buffers.
+
+---
+ nmz/search.c | 12 ++++++------
+ 1 file changed, 6 insertions(+), 6 deletions(-)
+
+diff --git a/nmz/search.c b/nmz/search.c
+index b8d015d..7c2e76a 100644
+--- a/nmz/search.c
++++ b/nmz/search.c
+@@ -569,7 +569,7 @@ do_regex_preprocessing(char *expr)
+ {
+     if (*expr == '*' && expr[strlen(expr) - 1] != '*') {
+         /* If suffix match such as '*bar', enforce it into regex */
+-        strcpy(expr, expr + 1);
++        memmove(expr, expr + 1, strlen(expr));
+         escape_meta_characters(expr, BUFSIZE * 2);
+         strncat(expr, "$", BUFSIZE * 2 - strlen(expr) - 1);
+         expr[BUFSIZE * 2 - 1] = '\0';
+@@ -581,7 +581,7 @@ do_regex_preprocessing(char *expr)
+         expr[BUFSIZE * 2 - 1] = '\0';
+     } else if (*expr == '*' && expr[strlen(expr) - 1] == '*') {
+         /* If internal match such as '*foo*', enforce it into regex */
+-        strcpy(expr, expr + 1);
++        memmove(expr, expr + 1, strlen(expr));
+         expr[strlen(expr) - 1] = '\0';
+         escape_meta_characters(expr, BUFSIZE * 2);
+     } else if (*expr == '/' && expr[strlen(expr) - 1] == '/') {
+@@ -589,7 +589,7 @@ do_regex_preprocessing(char *expr)
+             nmz_debug_printf("do REGEX search\n");
+             /* Genuine regex */
+             /* Remove the both of '/' chars at begging and end of string */
+-            strcpy(expr, expr + 1);
++            memmove(expr, expr + 1, strlen(expr));
+             expr[strlen(expr) - 1]= '\0';
+         } else {
+             nmz_debug_printf("disabled REGEX search\n");
+@@ -602,7 +602,7 @@ do_regex_preprocessing(char *expr)
+             || (*expr == '{' && expr[strlen(expr) - 1] == '}')) 
+         {
+             /* Delimiters of field search */
+-            strcpy(expr, expr + 1); 
++            memmove(expr, expr + 1, strlen(expr)); 
+             expr[strlen(expr) - 1] = '\0';
+         }
+         escape_meta_characters(expr, BUFSIZE * 2);
+@@ -692,7 +692,7 @@ static void
+ delete_beginning_backslash(char *str)
+ {
+     if (*str == '\\') {
+-        strcpy(str, str + 1);
++        memmove(str, str + 1, strlen(str));
+     }
+ }
+ 
+@@ -932,7 +932,7 @@ void remove_quotes(char *str)
+     if ((strlen(str) >= 3 && (*str == '"' && str[strlen(str) - 1] == '"'))
+         || (*str == '{' && str[strlen(str) - 1] == '}')) 
+     {
+-        strcpy(str, str + 1); 
++        memmove(str , str + 1, strlen(str)); 
+         str[strlen(str) - 1]= '\0';
+     } 
+ }
